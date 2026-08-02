efecc6f4a0e9f14d480713a51c4ea2fe87c8ae8b
juhanah25-art <juhanah25@guru.sd.belajar.id>
2026-08-02 11:08:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// script.js
+function updateJam(){
+
+const sekarang=new Date();
+
+document.getElementById("jam").innerHTML=
+sekarang.toLocaleDateString("id-ID")+
+"<br>"+
+sekarang.toLocaleTimeString("id-ID");
+
+}
+
+setInterval(updateJam,1000);
+
+updateJam();
+
+document
+.getElementById("btnTes")
+.onclick=function(){
+
+alert("Website GitHub berhasil!");
+
+}
