67ef009f11b82ddfd1ba367d7e3472fda07adf3d
juhanah25-art <juhanah25@guru.sd.belajar.id>
2026-08-02 12:04:38 +0700

    Refactor updateJam and onScanSuccess functions

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
-function updateJam(){
+const API = "https://script.google.com/macros/s/AKfycbyAzRESS17oc5ABmxl9--Nd1yoER3E0kzryWecBK7FVW9xPJos7GAW56ldxYb6_J3fOJA/exec";
 
-const sekarang=new Date();
+let idGuru = "";
+let namaGuru = "";
 
-document.getElementById("jam").innerHTML=
-sekarang.toLocaleDateString("id-ID")+
-"<br>"+
-sekarang.toLocaleTimeString("id-ID");
+function updateJam() {
+  const sekarang = new Date();
 
+  document.getElementById("jam").innerHTML =
+    sekarang.toLocaleDateString("id-ID") +
+    "<br>" +
+    sekarang.toLocaleTimeString("id-ID");
 }
 
-setInterval(updateJam,1000);
-
+setInterval(updateJam, 1000);
 updateJam();
 
-document
-.getElementById("btnTes")
-.onclick=function(){
+function onScanSuccess(decodedText) {
 
-alert("Website GitHub berhasil!");
+  idGuru = decodedText;
 
-}
-function onScanSuccess(decodedText){
+  document.getElementById("idGuru").innerHTML = decodedText;
 
-document.getElementById("hasilQR").innerHTML=
-"QR : "+decodedText;
+  fetch(API + "?aksi=guru&id=" + encodeURIComponent(decodedText))
+    .then(res => res.json())
+    .then(data => {
 
-}
+      if (data.status) {
 
-let html5QrCode = new Html5Qrcode("reader");
+        namaGuru = data.nama;
 
-Html5Qrcode.getCameras().then(devices=>{
+        document.getElementById("namaGuru").innerHTML = data.nama;
 
-if(devices.length){
+      } else {
 
-html5QrCode.start(
+        document.getElementById("namaGuru").innerHTML = "Guru tidak ditemukan";
 
-devices[0].id,
+      }
 
-{
-fps:10,
-qrbox:250
-},
-
-onScanSuccess
-
-);
+    });
 
 }
-
-});
