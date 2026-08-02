49ba62e96d35b4ebd322864c8bae913393ac978f
juhanah25-art <juhanah25@guru.sd.belajar.id>
2026-08-02 11:38:24 +0700

    Implement QR code scanning functionality

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ document
 alert("Website GitHub berhasil!");
 
 }
+function onScanSuccess(decodedText){
+
+document.getElementById("hasilQR").innerHTML=
+"QR : "+decodedText;
+
+}
+
+let html5QrCode = new Html5Qrcode("reader");
+
+Html5Qrcode.getCameras().then(devices=>{
+
+if(devices.length){
+
+html5QrCode.start(
+
+devices[0].id,
+
+{
+fps:10,
+qrbox:250
+},
+
+onScanSuccess
+
+);
+
+}
+
+});
